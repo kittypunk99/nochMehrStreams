fcc317d0c573bf2cf90cb7078ecd958acf8adea9
LlordZombie <linus.freistetter@hotmail.com>
2025-02-24 22:35:00 +0100

    eierlecjen

--- src/logfiles/LogStreams.java
+++ src/logfiles/LogStreams.java
@@ -3,10 +3,7 @@ package logfiles;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.Comparator;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class LogStreams {
